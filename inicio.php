5bab44b5707dcf582aa1d99103455d4238dc0eae
Ana Luisa <analuisacomisrocha2@gmail.com>
2026-06-30 17:02:47 -0300

    Add files via upload

---
+++ inicio.php
@@ -0,0 +1,24 @@
+<?php 
+//inclui o arquivo responsavel pela conexao
+
+include "conexao.php";
+
+$num1 = 10;
+$num2 = 13;
+$num3 = 20;
+$resultado = $num1 + $num2;
+
+//linguagem sql
+
+$sql ="INSERT INTO calculo(numero1, numero2, resultado)VALUES($num1, $num2, $resultado)";
+
+if ($conexao->query($sql)){
+   echo "<br> Dados salvo com sucesso!";
+    
+}else{
+    echo "<br> Erro ao salvar";
+}
+
+
+
+?>
